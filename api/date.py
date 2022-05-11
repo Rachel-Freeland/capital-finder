7c0d176278ace2326ba185faddb931fe9e2fda69
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-11 12:03:31 -0500

    playing with code for a moment

--- api/date.py
+++ api/date.py
@@ -9,4 +9,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        self.wfile.write(str(datetime.now().strftime("Hello, world!")).encode())
         return
